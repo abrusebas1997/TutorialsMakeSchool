93e423ce69ef4f0875a5462fc8b8a5e34dd2f516
Sebastian Abarca <49495659+abrusebas1997@users.noreply.github.com>
2021-07-26 18:56:02 -0700

    Update dog.py

--- superhero/dog.py
+++ superhero/dog.py
@@ -4,5 +4,7 @@ class Dog:
         self.name = name
         print("dog initialized!")
 
-#Instantiation
-Dog("Rex")
+#Instantiation in a variable
+my_dog = Dog("Rex")
+print(my_dog)
+print(my_dog.name)
